e2e9859eec0abeb06d637e50041cf59a04ecd3a9
Tulluru Dinesh <dineshtulluru1020@gmail.com>
2025-09-04 12:55:31 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { viteSourceLocator } from "@metagptx/vite-plugin-source-locator";
+export default defineConfig(({ mode }) => ({
+  
+  plugins: [
+    viteSourceLocator({
+      prefix: "mgx",
+    }),
+    react(),
+    
+  ],
+  server: {
+    port: 3000, 
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
